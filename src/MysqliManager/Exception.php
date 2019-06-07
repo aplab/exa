0e0500fb19c5cf9055bedcf45d01fd4f9b48875d
polyanin <polyanin@gmail.com>
2019-06-07 22:29:16 +0500

    configure services

---
+++ src/MysqliManager/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 14.09.2018
+ * Time: 9:43
+ */
+
+namespace App\MysqliManager;
+
+/**
+ * Class Exception
+ * @package App\MysqliManager
+ */
+class Exception extends \Exception
+{
+
+}
